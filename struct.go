ca1c9e76a2368ece1ed1fc46bdf68e98d9df7489
HunKiat <hunkiat@gmail.com>
2022-03-29 18:15:57 +0800

    first init

--- struct.go
+++ struct.go
@@ -1,6 +1,30 @@
 package main
 
+// developing only uncompleted...
 //Insert struct declaration here
+type jediKnight struct {
+	FName            string  `json:"fname"`
+	LName            string  `json:"lName"`
+	Age              int     `json:"age"`
+	Subscriber       bool    `json:"subscriber"`
+	Phone            int     `json:"phone"`
+	CreditAvailable  string  `json:"creditAvailable"`
+	CurrentCartCost  float32 `json:"currentCartCost"`
+	CurrentOrderCost float32 `json:"currentOrderCost"`
+}
+
+var jedis = []jediKnight{}
+
+// coruscant is of world of all Jedis
+type coruscant struct {
+	// entry interface{}
+	entry *jediKnight
+	next  *coruscant
+}
+
+type JediWorld struct {
+	head *coruscant
+}
 
 func main() {
 	//Insert code here
